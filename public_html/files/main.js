15bfae645d9ddf9b97da77937bb3f9c624d11734
Jimmy Liu <sicong.liu98@gmail.com>
2014-01-16 22:15:41 -0800

    Cleanup

---
+++ public_html/files/main.js
@@ -0,0 +1,17 @@
+var cookie;
+var cookieCountText;
+var clicks;
+
+window.onload = function() {
+    Initialize();
+};
+
+function Initialize() { 
+    // What will we put here?
+    
+}
+
+function clicked() {
+    // What about here?
+}
+
